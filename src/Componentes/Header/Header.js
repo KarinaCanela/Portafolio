beb22a95b7ab45cb29d2f5551f74b7f76d548c33
karina <kcanelacobos@gmail.com>
2023-07-28 19:29:23 -0600

    Se modifico el archivo Header

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,30 +1,68 @@
 import './Header.css'
 
 function Header () {
-    return <nav class="nav">
-    <div class="container">
-        <h1 class="logo"><img src="/img/vector.png"/><a href="#">Karla Karina Canela Cobos</a></h1>
-        <ul>
-            <li><a href="#" class="current">Sobre mi</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Hobbies</a></li>
-            <li><a href="#">Formacion</a></li>
-            <li><a href="#">Proyectos</a></li>
-            <li><a href="#">Contacto</a></li>
-        </ul>
-    </div>
-    </nav>
+    return <nav>
+    <ul class="menu">
+      <li class="logo"><a href="#">Invisible App</a></li>
+      <li class="item"><a href="#">Home</a></li>
+      <li class="item"><a href="#">About</a></li>
+      <li class="item"><a href="#">Services</a></li>
+      <li class="item"><a href="#">Features</a></li>
+      <li class="item"><a href="#">Blog</a></li>
+      <li class="item"><a href="#">Contact</a>
+      </li>
+      <li class="item button"><a href="#">Log In</a></li>
+      <li class="item button secondary"><a href="#">Sign Up</a></li>
+      <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+    </ul>
+  </nav>
 }
 
-const nav = document.querySelector(".nav");
-window.addEventListener("scroll", fixNav);
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleElements = document.querySelectorAll(".toggle");
 
-function fixNav() {
-    if (window.scrollY > nav.offsetHeight) {
-        nav.classList.add("active");
-    } else {
-        nav.classList.remove("active");
-    }
-}
+    toggleElements.forEach(function(toggleElement) {
+      toggleElement.addEventListener("click", function() {
+        const itemElements = document.querySelectorAll(".item");
 
-export default Header
+        if (itemElements[0].classList.contains("active")) {
+          itemElements.forEach(function(itemElement) {
+            itemElement.classList.remove("active");
+          });
+          this.querySelector("i").classList.remove("fa-times");
+          this.querySelector("i").classList.add("fa-bars");
+        } else {
+          itemElements.forEach(function(itemElement) {
+            itemElement.classList.add("active");
+          });
+          this.querySelector("i").classList.remove("fa-bars");
+          this.querySelector("i").classList.add("fa-times");
+        }
+      });
+    });
+  });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const toggleElements = document.querySelectorAll(".toggle");
+
+//     toggleElements.forEach(function(toggleElement) {
+//         toggleElement.addEventListener("click", function() {
+//             const itemElements = document.querySelectorAll(".item");
+
+//             if (itemElements[0].classList.contains("active")) {
+//                 itemElements.forEach(function(itemElement) {
+//                     itemElement.classList.remove("active");
+//                 });
+//                 this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+//             } else {
+//                 itemElements.forEach(function(itemElement) {
+//                     itemElement.classList.add("active");
+//                 });
+//                 this.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+//             }
+//         });
+//     });
+// });
+    
+
+export default Header;
