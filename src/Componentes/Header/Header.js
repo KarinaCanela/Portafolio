3041d35842d4732aefff302e1cf48434e94d55d0
karina <kcanelacobos@gmail.com>
2023-07-31 21:12:58 -0600

    Modificacion al menu de navegacion, responsivo

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -2,67 +2,43 @@ import './Header.css'
 
 function Header () {
     return <nav>
-    <ul class="menu">
-      <li class="logo"><a href="#">Invisible App</a></li>
-      <li class="item"><a href="#">Home</a></li>
-      <li class="item"><a href="#">About</a></li>
-      <li class="item"><a href="#">Services</a></li>
-      <li class="item"><a href="#">Features</a></li>
-      <li class="item"><a href="#">Blog</a></li>
-      <li class="item"><a href="#">Contact</a>
-      </li>
-      <li class="item button"><a href="#">Log In</a></li>
-      <li class="item button secondary"><a href="#">Sign Up</a></li>
-      <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+    <ul className="menu">
+      <li className="logo"><a href="#">Karla Karina Canela Cobos</a></li>
+      <li className="item"><a href="#">Sobre mi</a></li>
+      <li className="item"><a href="#">Skills</a></li>
+      <li className="item"><a href="#">Hobbies</a></li>
+      <li className="item"><a href="#">Formación</a></li>
+      <li className="item"><a href="#">Proyectos</a></li>
+      <li className="item"><a href="#">Contacto</a></li>
+      <li className="item button"><a href="https://github.com/KarinaCanela"><i className="fab fa-github fa-lg" title='GitHub'></i></a></li>
+      <li className="item button"><a href="https://www.linkedin.com/in/karla-karina-canela-cobos-81a83a183/"><i class="fab fa-linkedin fa-lg" title='LinkeIn'></i></a></li>
+      <li className="item button"><a href="https://instagram.com/karina_canela10?igshid=ZDdkNTZiNTM="><i class="fab fa-instagram fa-lg" title='Instagram'></i></a></li>
+      <li className="item button"><a href="img/Karla_Karina_Canela_Cobos_cv.pdf"><i class="fas fa-file-alt fa-lg" title='Curriculum'></i></a></li>
+      <li className="toggle"><a href="#" ><i id='btnmenu' className="fas fa-bars"></i></a></li>
     </ul>
   </nav>
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleElements = document.querySelectorAll(".toggle");
-
-    toggleElements.forEach(function(toggleElement) {
-      toggleElement.addEventListener("click", function() {
-        const itemElements = document.querySelectorAll(".item");
-
-        if (itemElements[0].classList.contains("active")) {
-          itemElements.forEach(function(itemElement) {
-            itemElement.classList.remove("active");
-          });
-          this.querySelector("i").classList.remove("fa-times");
-          this.querySelector("i").classList.add("fa-bars");
-        } else {
-          itemElements.forEach(function(itemElement) {
-            itemElement.classList.add("active");
-          });
-          this.querySelector("i").classList.remove("fa-bars");
-          this.querySelector("i").classList.add("fa-times");
-        }
-      });
-    });
+window.addEventListener("load", function() {
+  const itemElements = document.getElementsByClassName("item");
+  const toggleIcon = document.getElementById("btnmenu");
+  
+  toggleIcon.addEventListener("click", function() {
+    if (itemElements[0].classList.contains("active")) {
+      for (let i = 0; i < itemElements.length; i++) {
+        itemElements[i].classList.remove("active");
+      }
+      toggleIcon.classList.remove("fa-times");
+      toggleIcon.classList.add("fa-bars");
+    } else {
+      for (let i = 0; i < itemElements.length; i++) {
+        itemElements[i].classList.add("active"); 
+      }
+      toggleIcon.classList.remove("fa-bars");
+      toggleIcon.classList.add("fa-times");
+    }
   });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const toggleElements = document.querySelectorAll(".toggle");
-
-//     toggleElements.forEach(function(toggleElement) {
-//         toggleElement.addEventListener("click", function() {
-//             const itemElements = document.querySelectorAll(".item");
-
-//             if (itemElements[0].classList.contains("active")) {
-//                 itemElements.forEach(function(itemElement) {
-//                     itemElement.classList.remove("active");
-//                 });
-//                 this.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-//             } else {
-//                 itemElements.forEach(function(itemElement) {
-//                     itemElement.classList.add("active");
-//                 });
-//                 this.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-//             }
-//         });
-//     });
-// });
+});
     
 
 export default Header;
