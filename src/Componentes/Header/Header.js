b8aab5a8b3205cc658fe7db1abeb5a5bb1cdf667
karina <kcanelacobos@gmail.com>
2023-08-01 17:20:31 -0600

    Modificacion del navbar

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 function Header () {
     return <nav>
     <ul className="menu">
-      <li className="logo"><a href="#">Karla Karina Canela Cobos</a></li>
+      <li className="logo"><a href="#"><i class="fas fa-laptop-code fa-lg"></i>Karla Karina Canela Cobos</a></li>
       <li className="item"><a href="#">Sobre mi</a></li>
       <li className="item"><a href="#">Skills</a></li>
       <li className="item"><a href="#">Hobbies</a></li>
@@ -13,7 +13,7 @@ function Header () {
       <li className="item button"><a href="https://github.com/KarinaCanela"><i className="fab fa-github fa-lg" title='GitHub'></i></a></li>
       <li className="item button"><a href="https://www.linkedin.com/in/karla-karina-canela-cobos-81a83a183/"><i class="fab fa-linkedin fa-lg" title='LinkeIn'></i></a></li>
       <li className="item button"><a href="https://instagram.com/karina_canela10?igshid=ZDdkNTZiNTM="><i class="fab fa-instagram fa-lg" title='Instagram'></i></a></li>
-      <li className="item button"><a href="img/Karla_Karina_Canela_Cobos_cv.pdf"><i class="fas fa-file-alt fa-lg" title='Curriculum'></i></a></li>
+      <li className="item button secondary"><a href="img/Karla_Karina_Canela_Cobos_cv.pdf"><i class="fas fa-file-alt fa-lg" title='Curriculum'></i></a></li>
       <li className="toggle"><a href="#" ><i id='btnmenu' className="fas fa-bars"></i></a></li>
     </ul>
   </nav>
