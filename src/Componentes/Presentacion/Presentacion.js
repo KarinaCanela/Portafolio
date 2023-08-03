8dbeabb0fa213ea6f17ab50d06068013db0ad0d6
karina <kcanelacobos@gmail.com>
2023-08-03 14:21:47 -0600

    Se modifico la seccion de Presentacion

--- src/Componentes/Presentacion/Presentacion.js
+++ src/Componentes/Presentacion/Presentacion.js
@@ -5,7 +5,7 @@ function Presentacion () {
         <img className='presentacion__profile' src='img/profile.jpg'/>
         <div className="presentacion__container">
             <h2 className="presentacion__bio">Hola, mi nombre es Karina y construyo paginas web.</h2>
-            <h3 className="presentacion__subsection">Soy formada en el Tecnológico de Estudios Superiores de Chalco con la carrera de Ingeniería en Sistemas Computacionales y actualmente estoy participando del proyecto Oracle ONE en Alura Latam con la especialidad en Front End.</h3>
+            <h3 className="presentacion__subsection">Soy formada en el Tecnológico de Estudios Superiores de Chalco con la carrera de Ingeniería en Sistemas Computacionales con especialidad en Desarrollo de Aplicaciones Web y Dispositivos Moviles y actualmente estoy participando en el proyecto Oracle ONE en Alura Latam con la especialidad en Front End.</h3>
         </div>
     </div> 
 }
